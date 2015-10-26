5570c6e4bc18fbcd8c6e010c5fbff05dc8e72bb0
Alfred Ababio <aababio75@gmail.com>
2015-10-25 20:38:22 -0400

    Can now draw Liquor only, but its gross, dont do it

--- soofa.js
+++ soofa.js
@@ -9,13 +9,14 @@ $( document ).ready(function() {
   };
 
   var foodAndDrinkFields = ['businessname', 'address', 'zip', 'licstatus', 'licenseadddttm', 'dayphn'];
-  var liqFields = ['address', 'dbaname', 'businessname', 'comments','opening', 'closing', 'expdttm', 'issdttm', 'licstatus', 'liccat', 'zip'];
+  var liqFields = ['address', 'dbaname', 'businessname', 'comments','opening', 'closing', 'expdttm', 'issdttm', 'licstatus', 'liccat', 'zip', 'city'];
 
   var foodAndDrinkMasterData = [];
   var liquorMasterData = [];
   var combinedMaster = [];
   var filteredFoodAndDrink = [];
   var filteredLiquor = [];
+  var $licenseBody = $('.licenses.col-md-9 > .row');
 
   var foodRequest = $.ajax({
     url : foodAndDrinkURL + '$select=' + foodAndDrinkFields.join(',') + '&$limit=50000',
@@ -33,13 +34,12 @@ $( document ).ready(function() {
     method : 'GET'
   });
 
-  $('#datepicker').datepicker();
+  $('#datepicker').datepicker()
+    .on('changeDate', function(e) {
+      readFilters();
+    });
   // Set the date to today.
-  $('#dateipicker').on("changeDate", function() {
-      $('#my_hidden_input').val(
-          console.log$('#datepicker').datepicker('getFormattedDate')
-      );
-  });
+  // $('#dateipicker').datepicker()
 
   // Wouldn't do this for massive datasets, pagination is optimization though
   Q.all([
@@ -94,23 +94,33 @@ $( document ).ready(function() {
     }
 
     var readFilters = function() {
+      var selectedType = $('#license-type-filter').find(':selected').text();
       switch (selectedType) {
         case 'Food Only':
+          clearBoard();
           render(filteredFoodAndDrink, 'food');
           break;
         case 'Liquor Only':
+          clearBoard();
           render(filteredLiquor, 'liq');
           break;
         default:
+          clearBoard();
           render(combinedMaster, 'both');
           render(filteredFoodAndDrink, 'food');
           render(filteredLiquor, 'liq');
       }
     };
 
+    var clearBoard = function() {
+      _.each($('.licenses.col-md-9 > .row').children('.col-md-4'), function($panelElm) {
+        $panelElm.remove();
+      });
+      console.log("Shit Removed");
+    }
     var render = function(array, type) {
       _.each(array, function(store) {
-        switch(type {
+        switch(type) {
           case 'food':
             drawFood(store);
             break;
@@ -122,13 +132,11 @@ $( document ).ready(function() {
             break;
           default:
             console.err("This error should never come up, maybe use if here.");
-        })
+        }
       });
     };
 
     var drawFood = function(foodPlace) {
-      var $licenseBody = $('.licenses.col-md-9 > .row');
-      var selectedType = $('#license-type-filter').find(':selected').text();
       var selectedDate = new Date($('#datepicker').datepicker('getDate'));
       var selectedDateMap = {
         year : selectedDate.getYear() + 1900,
@@ -144,7 +152,7 @@ $( document ).ready(function() {
       };
 
       var isLicensed = hasActiveFoodLicense(placeDateMap, selectedDateMap);
-      var licensedGlyph = if (isLicensed) ? 'ok' : 'remove';
+      var licensedGlyph = (isLicensed) ? 'ok' : 'remove';
 
       var $panelElement = $('<div class="col-md-4"><div class="panel panel-default">'
         + '<div class="panel-heading"><h3 class="panel-title">' + foodPlace.businessname 
@@ -152,8 +160,46 @@ $( document ).ready(function() {
         + '<br/>' + foodPlace.city + ', MA ' + foodPlace.zip
         + '<br/></address>Food License? <span class="glyphicon glyphicon-' + licensedGlyph
         + '" aria-hidden="true"></span><p class="license-date">Issued on: <span aria-hidden="true">'
-        + placeDateMap.month + '-' placeDateMap.day + '-' placeDateMap.year + '</span></p>'
+        + placeDateMap.month + '-' + placeDateMap.day + '-' + placeDateMap.year + '</span></p>'
         + '</div></div></div>');
+      $licenseBody.append($panelElement);
+    };
+
+    var drawLiquor = function(liqPlace) {
+      var selectedDate = new Date($('#datepicker').datepicker('getDate'));
+      var selectedDateMap = {
+        year : selectedDate.getYear() + 1900,
+        month : selectedDate.getMonth() + 1,
+        day : selectedDate.getDate()
+      };
+
+      var placeStartDateUnFormatted = liqPlace.issdttm.substring(0, 10);
+      var placeStartDateMap = {
+        year : parseInt(placeStartDateUnFormatted.substring(0, 4)),
+        month : parseInt(placeStartDateUnFormatted.substring(5, 7)),
+        day : parseInt(placeStartDateUnFormatted.substring(8, 10))
+      };
+
+      var placeEndDateUnFormatted = liqPlace.expdttm.substring(0, 10);
+      var placeEndDateMap = {
+        year : parseInt(placeEndDateUnFormatted.substring(0, 4)),
+        month : parseInt(placeEndDateUnFormatted.substring(5, 7)),
+        day : parseInt(placeEndDateUnFormatted.substring(8, 10))
+      };
+
+      var isLicensed = hasActiveLiquorLicense(placeStartDateMap, selectedDateMap, placeEndDateMap);
+      var menuSelections = getMenuSelection(liqPlace.liccat);
+
+      var $panelElement = $('<div class="col-md-4"><div class="panel panel-default">'
+        + '<div class="panel-heading"><h3 class="panel-title">' + liqPlace.businessname 
+        + '</h3></div></div class="panel-body"><address>' + liqPlace.address
+        + '<br/>' + liqPlace.city + ', MA ' + liqPlace.zip + '<br/></address>'
+        + menuSelections + '<p class="license-date">Issued on: <span aria-hidden="true">'
+        + placeStartDateMap.month + '-' + placeStartDateMap.day + '-' + placeStartDateMap.year
+        + '</span></p><p class="license-date">Expires on: <span aria-hidden="true">'
+        + placeEndDateMap.month + '-' + placeEndDateMap.day + '-' + placeEndDateMap.year
+        + '</span></p></div></div></div>');
+      $licenseBody.append($panelElement);
     };
 
     var hasActiveFoodLicense = function(placeDateMap, currentDateMap) {
@@ -162,5 +208,20 @@ $( document ).ready(function() {
         ((placeDateMap.year === currentDateMap.year) && (placeDateMap.month === currentDateMap.month) && (placeDateMap.day < currentDateMap.day)));
     };
 
+    var hasActiveLiquorLicense = function(startDateMap, currentDateMap, endDateMap) {
+      return (((startDateMap.year < currentDateMap.year) && (endDateMap.year > currentDateMap.year)) ||
+        ((startDateMap.year === currentDateMap.year) && (endDateMap.year >= currentDateMap.year) && (startDateMap.month < currentDateMap.month)) ||
+        ((startDateMap.year === currentDateMap.year) && (endDateMap.year === currentDateMap.year) && (startDateMap.month <= currentDateMap.month) && (expMonth >= currentDateMap.month)) ||
+        ((startDateMap.year === currentDateMap.year) && (endDateMap.year === currentDateMap.year) && (startDateMap.month === currentDateMap.month) && (expMonth >= currentDateMap.month)) ||
+        ((startDateMap.year === currentDateMap.year) && (endDateMap.year === currentDateMap.year) && (startDateMap.month === currentDateMap.month) && (expMonth === currentDateMap.month) && (startDateMap.day <= currentDateMap.day) && (endDateMap.day >= currentDateMap.day)));
+    }
+
+    var getMenuSelection = function(licCat) {
+      if(_.contains(licCatMap['All Alcoholic'], licCat)) {
+        return 'All Alcoholic Beverages';
+      } else if(_.contains(licCatMap['Wine and Malts Only'], licCat)) {
+        return 'Wine and Malts Only';
+      }
+    }
 
 });
